5df303d811d038f4d5c6b2c0f752ac4693eb689f
Aaron Edwards <aaron@cloudgenix.com>
2020-08-14 10:53:05 -0700

    1.0.0b1 commit

---
+++ optimize_acl.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+from cloudgenix_tenant_acl import optimize_acl
+
+if __name__ == "__main__":
+    optimize_acl()
---
+++ pull_acl.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+from cloudgenix_tenant_acl.pull import go
+
+if __name__ == "__main__":
+    go()
